2650377a91903f13cb7da5a48369e1b8b36a3122
Sara Sychowska <sara.sychowska@zst.info.pl>
2021-10-13 09:10:21 +0200

    Version 1.0

---
+++ Pokemons/Pokemon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemons
+{
+    class Pokemon
+    {
+        private static string name;
+        private List<Skill> Skills;
+        private static string Type;
+        private static int hP;
+        private static int level;
+
+        static void Constructor()
+        {
+            level = 1;
+        }
+
+        static void displayName()
+        {
+            Console.WriteLine(name);
+        }
+
+        static void listSkills()
+        {
+            return Skills();
+        }
+
+        static void getSkill(int index)
+        {
+            return Skill;
+        }
+
+        static void showStats(string[] args)
+        {
+
+        }
+
+        static void getHP()
+        {
+            return hP;
+        }
+
+        static void getLevel()
+        {
+            return level;
+        }
+
+    }
+}
---
+++ Pokemons/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemons
+{
+    class Skill
+    {
+        public string name;
+        public int BaseDMG;
+        public string Type;
+
+        static void Constructor()
+        {
+
+        }
+    }
+}
