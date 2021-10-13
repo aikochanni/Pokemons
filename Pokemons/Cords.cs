02a8e768eee4b85f059817c0ea2ef74600d13abe
Sara Sychowska <sara.sychowska@zst.info.pl>
2021-10-13 12:03:21 +0200

    Version 1.4

--- Pokemons/Cords.cs
+++ Pokemons/Cords.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Pokemons
 {
-    class Cords
+    public class Cords
     {
         public double X;
         public double Y;
--- Pokemons/Trainer.cs
+++ Pokemons/Trainer.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pokemons
 {
-    class Trainer
+    public class Trainer
     {
+        private string Name;
+        private List<Pokemon> Pokedex;
+        private List<Pokemon> Team;
+        private Cords Location;
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Pokedex = new List<Pokemon>();
+            Team = new List<Pokemon>();
+        }
+
+        public void DisplayName()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public List<Pokemon> GetPokemons()
+        {
+            return Pokedex;
+        }
+
+        public void Catch(Pokemon)
+        {
+            if (Team.Capacity() > 6)
+            {
+                Pokedex.Add(new Pokemon());
+            } else if (Team.Capacity() < 6)
+            {
+                Team.Add(new Pokemon());
+                Pokedex.Add(new Pokemon());
+            }
+        }
+
+        public void Release(int index)
+        {
+            Team.Remove(Pokemon[index]);
+        }
+
+        public List<Pokemon> GetTeam()
+        {
+            return Team;
+        }
+
+        public void DisplayTeam()
+        {
+            Team.ForEach(num => Console.WriteLine(num + ", "));
+            Console.WriteLine();
+        }
+
+        public void MoveBy(double x, double y)
+        { 
+            double x2 = 8;
+            double y2 = 10;
+            var sum = (x + y) + (x2 + y2);
+        }
     }
 }
